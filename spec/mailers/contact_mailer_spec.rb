3220523ff3dbbd0f5c105efc7e67552268ff0ddd
Maxim <maxim.kuepper@gmail.com>
2014-02-18 13:41:31 +0100

    renamed travis config as it is not longer for oauth only

--- spec/mailers/contact_mailer_spec.rb
+++ spec/mailers/contact_mailer_spec.rb
@@ -1,4 +1,13 @@
 require "spec_helper"
 
 describe ContactMailer do
+  describe "contact_mailer" do
+    let(:contact) { mock_model Contact, email: "spec@example.com", name: "Spec Test", subject: "Spec Test Subject", text: "Lorem  Ipsum"}
+    let(:mail) { ContactMailer.contact_email(contact) }
+
+    it {  mail.from.should include(Figaro.env.noreply_email) }
+    it {  mail.to.should include(Figaro.env.contact_email) }
+    it {  mail.subject.should eq("Spec Test Subject") }
+    it {  mail.subject.should eq("Spec Test Subject") }
+  end
 end
