18407ecdd7ca18aeb983e6c1be559bee655b97d0
Maxim <maxim.kuepper@gmail.com>
2014-02-22 14:39:56 +0100

    fix for sending mails twice

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Koelschbusters::Application.configure do
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.sendmail_settings = {
         location:  '/usr/sbin/sendmail',
-        arguments: '-i -t'
+        arguments: '-i'
   }
   config.action_mailer.default_url_options = { host: 'www.koelschbusters.de' }
 end
