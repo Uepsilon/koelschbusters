5ad96427cb8e5a94bd4020f8dba478d0520abf89
Maxim Küpper <mku@i22.de>
2014-03-31 09:59:44 +0200

    adds missing module for slugification

---
+++ app/concerns/slugify.rb
@@ -0,0 +1,9 @@
+# encoding: UTF-8
+module Slugify
+  def self.slugify unslugged
+    slug = unslugged.to_s
+    slug.gsub!(/%/, ' prozent')
+    slug.gsub!(/€/, ' euro')
+    slug.parameterize
+  end
+end
