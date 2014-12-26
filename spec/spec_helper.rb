f939a00c4a311540d95427610c83295135ba7aaa
Maxim Küpper <maxim.kuepper@i22.de>
2014-12-26 22:19:01 +0100

    formats rspec output

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,12 @@ require 'rspec/autorun'
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
