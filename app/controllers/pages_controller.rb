c0cf5568b046aa6b1d4d192f5469b974071495cc
Maxim Küpper <maxim@railslove.com>
2016-03-13 13:53:09 +0100

    updates carnival float page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,7 @@
 class PagesController < ApplicationController
+
+  def carnival_float
+    @stories = YAML.load_file(Rails.root.join('config', 'carnival_floats.yml'));
+    @stories = @stories['carnival_floats']
+  end
 end
