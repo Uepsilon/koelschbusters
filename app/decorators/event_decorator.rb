41de06a5cd6f7afb6f7a863d58af70eeec9ddc49
Maxim Küpper <maxim@railslove.com>
2016-03-24 23:09:31 +0100

    improves event layout

--- app/decorators/event_decorator.rb
+++ app/decorators/event_decorator.rb
@@ -2,10 +2,10 @@ class EventDecorator < Draper::Decorator
   delegate_all
 
   def end_date
-    h.l object.ends_at, format: :short unless object.ends_at == object.starts_at
+    h.l object.ends_at, format: '%d.%m.%Y %H:%M Uhr' unless object.ends_at == object.starts_at
   end
 
   def start_date
-    h.l object.starts_at, format: :short
+    h.l object.starts_at, format: '%d.%m.%Y %H:%M Uhr'
   end
 end
--- spec/decorators/event_decorator_spec.rb
+++ spec/decorators/event_decorator_spec.rb
@@ -4,10 +4,10 @@ describe EventDecorator do
   let!(:event) { create(:event).decorate }
   let!(:event_without_end_date) { create(:event, ends_at: nil).decorate }
 
-  it { expect(event.start_date).to eq I18n.l(event.starts_at, format: :short) }
+  it { expect(event.start_date).to eq I18n.l(event.starts_at, format: '%d.%m.%Y %H:%M Uhr') }
   it { expect(event.end_date).to_not be_blank }
-  it { expect(event.end_date).to eq I18n.l(event.ends_at, format: :short) }
+  it { expect(event.end_date).to eq I18n.l(event.ends_at, format: '%d.%m.%Y %H:%M Uhr') }
 
-  it { expect(event_without_end_date.start_date).to eq I18n.l(event_without_end_date.starts_at, format: :short) }
+  it { expect(event_without_end_date.start_date).to eq I18n.l(event_without_end_date.starts_at, format: '%d.%m.%Y %H:%M Uhr') }
   it { expect(event_without_end_date.end_date).to be_blank }
 end
